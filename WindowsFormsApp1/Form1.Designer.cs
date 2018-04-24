b4b5e7c8d877854f105ed945ab9ce11eb0586a20
CJHart02 <carlton.hart@gmail.com>
2018-04-24 16:32:52 -0400

    Added more text boxes and stuff.

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -44,6 +44,7 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -88,7 +89,7 @@
             // Browse
             // 
             this.Browse.Font = new System.Drawing.Font("Comic Sans MS", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Browse.Location = new System.Drawing.Point(397, 33);
+            this.Browse.Location = new System.Drawing.Point(543, 47);
             this.Browse.Name = "Browse";
             this.Browse.Size = new System.Drawing.Size(75, 23);
             this.Browse.TabIndex = 4;
@@ -99,7 +100,7 @@
             // Search
             // 
             this.Search.Font = new System.Drawing.Font("Comic Sans MS", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Search.Location = new System.Drawing.Point(478, 34);
+            this.Search.Location = new System.Drawing.Point(624, 48);
             this.Search.Name = "Search";
             this.Search.Size = new System.Drawing.Size(75, 23);
             this.Search.TabIndex = 5;
@@ -138,11 +139,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(17, 103);
+            this.label1.Location = new System.Drawing.Point(12, 103);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(324, 20);
+            this.label1.Size = new System.Drawing.Size(447, 20);
             this.label1.TabIndex = 9;
-            this.label1.Text = "Please select the animal you are searching for";
+            this.label1.Text = "Please select the animals you are searching for and click search";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // openFileDialog1
@@ -153,18 +154,17 @@
             // textBox1
             // 
             this.textBox1.ForeColor = System.Drawing.Color.Black;
-            this.textBox1.Location = new System.Drawing.Point(21, 34);
+            this.textBox1.Location = new System.Drawing.Point(21, 48);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(370, 22);
+            this.textBox1.Size = new System.Drawing.Size(516, 22);
             this.textBox1.TabIndex = 10;
-            this.textBox1.Text = "Please enter browse for or type full path to file";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(458, 103);
+            this.label2.Location = new System.Drawing.Point(486, 103);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(213, 20);
             this.label2.TabIndex = 11;
@@ -173,30 +173,41 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(478, 200);
+            this.textBox2.Location = new System.Drawing.Point(543, 200);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 22);
             this.textBox2.TabIndex = 12;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(478, 346);
+            this.textBox3.Location = new System.Drawing.Point(543, 346);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 22);
             this.textBox3.TabIndex = 13;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(478, 493);
+            this.textBox4.Location = new System.Drawing.Point(543, 493);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 22);
             this.textBox4.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(270, 17);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Please enter full path or browse to image.";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(711, 584);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
@@ -238,6 +249,7 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
     }
 }
 
